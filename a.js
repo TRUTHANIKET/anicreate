af3d4a7ec513a720fa3cebd1f9428e6e88e7d351
ANIKET AAGE NAHI PATA <100013837+TRUTHANIKET@users.noreply.github.com>
2022-08-18 18:12:50 +0530

    created gsap anim
    yeah buddy

---
+++ a.js
@@ -0,0 +1,5 @@
+gsap.from('.bigtex',{duration:1,y:30,opacity:0,ease:'power2.in'})
+
+gsap.from('.smoltex',{duration:1,y:20,opacity:0,delay:1,ease:'power2.in'})
+gsap.from('.navi',{duration:1,y:-30,opacity:0,delay:0.5,ease:'power2.in'})
+gsap.from('#battan',{duration:1,y:-30,opacity:0,delay:2,ease:'power2.in'})
